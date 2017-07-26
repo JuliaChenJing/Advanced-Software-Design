c690ae2772e3746d24170ee9da0a89500ea81e6b
Julia Chen <juliachenjing@gmail.com>
2017-07-25 22:56:54 -0500

    commandpattern

--- CommandProjectSolution/src/bank/Application.java
+++ CommandProjectSolution/src/bank/Application.java
@@ -12,61 +12,58 @@ import bank.service.IAccountService;
 import bank.service.TransferFundsCommand;
 import bank.service.WithdrawCommand;
 
-
 public class Application {
 	public static void main(String[] args) {
-		//create HistoryList
+
 		HistoryList historyList = new HistoryList();
-		
 		IAccountService accountService = new AccountService();
+
 		// create 2 accounts;
-		accountService.createAccount(1263862, "Frank Brown");
-		accountService.createAccount(4253892, "John Doe");
-		//use account 1;
-		DepositCommand depositCommand1 = new DepositCommand(accountService,1263862, 240);
+		accountService.createAccount(1263862, "Julia Chen");
+		accountService.createAccount(4253892, "Bimal Parajuli");
+
+		// use account 1;
+		DepositCommand depositCommand1 = new DepositCommand(accountService, 1263862, 240);
 		depositCommand1.execute();
 		historyList.addCommand(depositCommand1);
 
-		DepositCommand depositCommand2 = new DepositCommand(accountService,1263862, 529);
+		DepositCommand depositCommand2 = new DepositCommand(accountService, 1263862, 529);
 		depositCommand2.execute();
 		historyList.addCommand(depositCommand2);
 
-		WithdrawCommand withdrawCommand1 = new WithdrawCommand(accountService,1263862, 529);
+		WithdrawCommand withdrawCommand1 = new WithdrawCommand(accountService, 1263862, 529);
 		withdrawCommand1.execute();
 		historyList.addCommand(withdrawCommand1);
 
-		//use account 2;
-		DepositCommand depositCommand3 = new DepositCommand(accountService,4253892, 12450);
+		// use account 2;
+		DepositCommand depositCommand3 = new DepositCommand(accountService, 4253892, 12450);
 		depositCommand3.execute();
 		historyList.addCommand(depositCommand3);
 
-		TransferFundsCommand transferFundsCommand1 = new TransferFundsCommand(accountService,4253892, 1263862, 100, "payment of invoice 10232");
+		TransferFundsCommand transferFundsCommand1 = new TransferFundsCommand(accountService, 4253892, 1263862, 100,
+				"payment of invoice 10232");
 		transferFundsCommand1.execute();
 		historyList.addCommand(transferFundsCommand1);
-		//undo
+
+		// undo
 		historyList.undo();
-		
-		// show balances		
+
+		// show balances
 		Collection<Account> accountlist = accountService.getAllAccounts();
 		Customer customer = null;
 		for (Account account : accountlist) {
 			customer = account.getCustomer();
 			System.out.println("Statement for Account: " + account.getAccountnumber());
 			System.out.println("Account Holder: " + customer.getName());
-			System.out.println("-Date-------------------------"
-							+ "-Description------------------"
-							+ "-Amount-------------");
+			System.out.println("--------------Date-------------------------" + "------Description---------------"
+					+ "-Amount-------------");
 			for (AccountEntry entry : account.getEntryList()) {
-				System.out.printf("%30s%30s%20.2f\n", entry.getDate()
-						.toString(), entry.getDescription(), entry.getAmount());
+				System.out.printf("%30s%30s%20.2f\n", entry.getDate().toString(), entry.getDescription(),
+						entry.getAmount());
 			}
-			System.out.println("----------------------------------------"
-					+ "----------------------------------------");
-			System.out.printf("%30s%30s%20.2f\n\n", "", "Current Balance:",
-					account.getBalance());
+			System.out.println("----------------------------------------" + "----------------------------------------");
+			System.out.printf("%30s%30s%20.2f\n\n", "", "Current Balance:", account.getBalance());
 		}
 	}
 
 }
-
-
