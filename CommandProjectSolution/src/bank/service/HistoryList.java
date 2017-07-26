cda2c092dbd625f3b5f6f639af10014d6e649a57
Julia Chen <juliachenjing@gmail.com>
2017-07-25 23:02:15 -0500

    review

--- CommandProjectSolution/src/bank/service/HistoryList.java
+++ CommandProjectSolution/src/bank/service/HistoryList.java
@@ -1,35 +1,33 @@
 package bank.service;
+
 import java.util.ArrayList;
 import java.util.Collection;
-
 
 public class HistoryList {
 
 	private Collection<ICommand> commandlist = new ArrayList();
 	private Collection<ICommand> undolist = new ArrayList();
 
-	public void undo(){	
-		if (commandlist.size()>0){
-		ICommand commandObject = (ICommand)((ArrayList)commandlist).get(commandlist.size()-1);
-		((ArrayList)commandlist).remove(commandObject);
-		commandObject.unExecute();
-		undolist.add(commandObject);
-		}
-	}
-    // this redo does not work correctly in all cases
-	// this is not part of the exercise
-	public void redo(){
-		if (undolist.size()>0){
-		ICommand commandObject = (ICommand)((ArrayList)undolist).get(undolist.size()-1);
-		commandObject.execute();
-		((ArrayList)undolist).remove(commandObject);
-		commandlist.add(commandObject);
+	public void undo() {
+		if (commandlist.size() > 0) {
+			ICommand commandObject = (ICommand) ((ArrayList) commandlist).get(commandlist.size() - 1);
+			((ArrayList) commandlist).remove(commandObject);//remove commandObject out of commandlist
+			commandObject.unExecute();
+			undolist.add(commandObject);
 		}
 	}
 
+	// this redo does not work correctly in all cases
+	public void redo() {
+		if (undolist.size() > 0) {
+			ICommand commandObject = (ICommand) ((ArrayList) undolist).get(undolist.size() - 1);
+			commandObject.execute();
+			((ArrayList) undolist).remove(commandObject);
+			commandlist.add(commandObject);
+		}
+	}
 
-
-	public void addCommand(ICommand commandObject){
+	public void addCommand(ICommand commandObject) {
 		commandlist.add(commandObject);
 	}
 
