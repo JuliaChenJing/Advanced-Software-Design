7e098153007f1cfd7a0684519622f8386cd0b8d1
Julia Chen <juliachenjing@gmail.com>
2017-08-06 19:33:26 -0700

    format

--- Chain/src/order/OrderHandler.java
+++ Chain/src/order/OrderHandler.java
@@ -1,7 +1,9 @@
 package order;
 
 public abstract class OrderHandler {
+	
 	protected OrderHandler nextHandler;
+	
 	public abstract void handleOrder(Order order);
 	
 	public void setNextHandler(OrderHandler nextHandler) {
