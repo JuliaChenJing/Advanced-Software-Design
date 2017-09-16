5a609713685336d2da39df2770cd225413251724
Julia Chen <juliachenjing@gmail.com>
2017-09-16 15:14:01 -0700

    dependency injection

--- SpringExerciseDILab/GreetingApplication/src/hello/HelloWorldApp.java
+++ SpringExerciseDILab/GreetingApplication/src/hello/HelloWorldApp.java
@@ -3,10 +3,17 @@ package hello;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloApp {
+public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");       
+		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");   
+		
+		/*
+		  <bean id="greetingService" class="hello.GreetingImpl">
+                 <property name="greeting"><value>Hello World</value></property>
+          </bean>
+		 */
+		
 		IGreeting greetingService = context.getBean("greetingService", IGreeting.class);
 		greetingService.sayHello();
 	}
