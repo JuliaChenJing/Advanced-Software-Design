68833f04778976bc59ce9aeeeb62273d9f225fdf
Julia Chen <juliachenjing@gmail.com>
2017-09-16 21:10:09 -0700

    bank

--- SpringExerciseDILab/BankProjectDISolution/src/bank/Application.java
+++ SpringExerciseDILab/BankProjectDISolution/src/bank/Application.java
@@ -13,22 +13,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");      
 		IAccountService accountService = (IAccountService)context.getBean("accountService");
 
 		// create 2 accounts;
-		accountService.createAccount(1263862, "Frank Brown");
-		accountService.createAccount(4253892, "John Doe");
+		accountService.createAccount(1263862, "Julia Chen");
+		accountService.createAccount(4253892, "Bimal Parajuli");
+		
 		//use account 1;
 		accountService.deposit(1263862, 240);
 		accountService.deposit(1263862, 529);
 		accountService.withdraw(1263862, 230);
+		
 		//use account 2;
 		accountService.deposit(4253892, 12450);
 		accountService.transferFunds(4253892, 1263862, 100, "payment of invoice 10232");
 		// show balances
 		
 		Collection<Account> accountlist = accountService.getAllAccounts();
+		
 		Customer customer = null;
 		for (Account account : accountlist) {
 			customer = account.getCustomer();
